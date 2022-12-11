0ca778b6a00233689ad6677f8ad06f89cd6639dd
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2022-12-11 10:20:01 +0800

    Update script.js

--- assets/mainpage/js/script.js
+++ assets/mainpage/js/script.js
@@ -1,20 +1,3 @@
-window.onload = function(){
-                var Input = document.getElementById("myInput");
-                var messageBox = document.getElementById("messageBox");
-            
-            function post(){
-                if(Input.value){
-                    var oTime = document.createElement("div");
-				    oTime.className = "time";
-				    var myDate = new  Date();
-				    oTime.innerHTML = myDate.toLocaleString();
-				    messageBox.appendChild(oTime);
-				
-                    var message = document.createElement("div");
-                    message.className="messageX";
-                    message.innerHTML=Input.value;
-                    Input.value="";
-                    messageBox.appendChild(message);
-                }
-            }
-        }
+function redirect(){
+	alert('hello!\n');
+}
