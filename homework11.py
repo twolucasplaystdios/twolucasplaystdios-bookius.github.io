1370f995178042585e8e511012ce45826f5de70f
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2022-12-31 13:24:04 +0000

    完成

--- homework11.py
+++ homework11.py
@@ -1,7 +1,8 @@
 import time
 
 day = 365 * 24 * 60 * 60
+timecount =  day - time.time
 
 while True:
-    print('離現在跨年的時間還有', day - time.time, '秒')
+    print('離現在跨年的時間還有', timecount, '秒')
     time.sleep(1)
