11967a3d44a537e926c8b3fd1bc807888a909eb6
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2023-01-11 18:36:58 +0800

    13156415612313

--- test.php
+++ test.php
@@ -2,4 +2,11 @@
     $data1 = $_GET["text_name"];
     $data2 = $_GET["text_title"];
     $data3 = $_GET["text_question"];
+    echo "<table width='3' border='1'>";
+    for ($i=0; $i<$columns; $i++) {
+        echo "<tr>名:$data1 </tr>";
+        echo "<tr>標題:$data2 </tr>";
+        echo "<tr>問題/投訴:$data3 </tr>";
+
+    }
 ?>
