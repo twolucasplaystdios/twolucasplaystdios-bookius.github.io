92baee4d0e40002be860f2df7713fda6ee8d7e37
twolucasplaystdios <97821497+twolucasplaystdios@users.noreply.github.com>
2023-01-11 19:23:40 +0800

    1

--- test.php
+++ test.php
@@ -1,12 +1,9 @@
 <?php
-    $data1 = $_GET["text_name"];
-    $data2 = $_GET["text_title"];
-    $data3 = $_GET["text_question"];
-    $data4 = $_GET["text_email"]
-    echo "<table width='3' border='1'>";
-    for ($i=0; $i<1; $i++) {
-        echo "<tr>名:$data1 </tr>";
-        echo "<tr>標題:$data2 </tr>";
-        echo "<tr>問題/投訴:$data3 </tr>";
-    }
+    echo $_GET['1'];
+    echo '<br />'
+    echo $_GET['2'];
+    echo '<br />'
+    echo $_GET['3'];
+    echo '<br />'
+    echo $_GET['4'];
 ?>
